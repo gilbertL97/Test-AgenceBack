3cfde025c5da2774a79ee2c1b10ab8c0425c2653
Gilbert Luis <gilbertluis97@gmail.com>
2025-09-29 05:41:12 -0300

    fix the group by by salario and comissao

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -37,20 +37,18 @@ class UserService
                 DB::raw('YEAR(f.data_emissao) as ano'),
                 DB::raw('MONTH(f.data_emissao) as mes'),
                 DB::raw('SUM(f.valor) as valor_total'),
-                DB::raw('SUM(f.total_imp_inc) as total_imp_porciento'), //numero expresado en porciento
-                DB::raw('SUM(f.valor) * (SUM(f.total_imp_inc) / 100)as valor_descontado'), //valor despues de multiplcar el porciento anterior al valor
-                DB::raw('f.comissao_cn as comision'),
-                DB::raw('SUM(f.valor) - (SUM(f.valor) * (SUM(f.total_imp_inc) / 100)) as receita_liquida'), //receita liquida valor total -descontado  RECEITA LIQUIDA = VALOR - TOTAL_IMP_INC
-                DB::raw('s.brut_salario as salario'), //aqui hiice ya lo del salario estatico para manterener la respues unida
-                DB::raw('(SUM(f.valor)-(SUM(f.valor) * (SUM(f.total_imp_inc) / 100)))*(f.comissao_cn /100)   as valor_comissao'), //Valor de comisión = (VALOR – (VALOR*TOTAL_IMP_INC)) * COMISSAO_CN
-                DB::raw('(SUM(f.valor) - (SUM(f.valor) * (SUM(f.total_imp_inc) / 100)))-(s.brut_salario-((SUM(f.valor)-(SUM(f.valor) * (SUM(f.total_imp_inc) / 100)))*(f.comissao_cn /100))) as lucro') //Lucro = (VALOR-TOTAL_IMP_INC) – (Costo fijo + comisión).
+                DB::raw('SUM(f.total_imp_inc) as total_imp_porciento'),
+                DB::raw('SUM(f.valor) * (SUM(f.total_imp_inc) / 100) as valor_descontado'),
+                DB::raw('MAX(f.comissao_cn) as comissao'), // <--- cambio aquí
+                DB::raw('SUM(f.valor) - (SUM(f.valor) * (SUM(f.total_imp_inc) / 100)) as receita_liquida'),
+                DB::raw('MAX(s.brut_salario) as salario'), // <--- cambio aquí
+                DB::raw('(SUM(f.valor)-(SUM(f.valor) * (SUM(f.total_imp_inc) / 100)))*(MAX(f.comissao_cn) /100) as valor_comissao'),
+                DB::raw('(SUM(f.valor) - (SUM(f.valor) * (SUM(f.total_imp_inc) / 100)))-(MAX(s.brut_salario)-((SUM(f.valor)-(SUM(f.valor) * (SUM(f.total_imp_inc) / 100)))*(MAX(f.comissao_cn) /100))) as lucro')
             )
             ->where('o.co_usuario', $idUser)
             ->whereBetween('f.data_emissao', [$startDate, $endDate])
             ->groupBy(
                 DB::raw('YEAR(f.data_emissao)'),
-                DB::raw('salario'),
-                DB::raw('comision'),
                 DB::raw('MONTH(f.data_emissao)')
             )
             ->orderBy('ano')
